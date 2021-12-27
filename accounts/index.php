640733d22fcd6f4acec321fee498d765c6b82870
mtunzisteven <mtunzism@byui.edu>
2021-12-27 07:11:25 +0200

    removed redundant session manager

--- accounts/index.php
+++ accounts/index.php
@@ -2,22 +2,6 @@
 
 //This is the Accounts Controller for the site
 
-
-// no session started var set yet = session just created 
-if(!isset($_SESSION['STARTED'])){
-
-    $_SESSION['STARTED'] = time();
-
-}else if(time()-$_SESSION['STARTED'] > 1800){
-    // session older than 30min
-    // change session id if session is older than 30 min
-    session_destroy();
-
-    // set new session started var
-    $_SESSION['STARTED'] = time();
-
-}
-
 // start session with same id in this file// start session with same id in this file
 require $_SERVER['DOCUMENT_ROOT'] . '/engoje/library/sessionsManager.php'; 
 
